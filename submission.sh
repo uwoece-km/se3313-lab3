5c5e6277823bede9ca77aff4221b9afa1784f407
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-10-20 15:11:26 -0400

    Add submission.sh file for submitting assignment zip file.

---
+++ submission.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+if [[ $# -ne "1" ]]; then
+  echo "Invalid arguments passed: $*"
+  echo "Usage: submission.sh \$userName"
+  exit 1
+fi
+
+git archive -o $1-lab2asn.zip --format zip --prefix $1/ HEAD
